e8f855a65242e8c805e5a6122bf6d63606592a00
Oleg Shchurov <o.shchurov@firecode-team.ru>
2026-04-21 22:09:55 +0300

    feat(route): realized routing between pages

--- src/components/LayoutArrows.vue
+++ src/components/LayoutArrows.vue
@@ -1,16 +1,71 @@
 <template>
   <nav class="layout-arrows" aria-label="Page navigation">
-    <button class="layout-arrows__button layout-arrows__button--left">
+    <RouterLink
+      v-if="previosPage"
+      :to="previosPage.path"
+      class="layout-arrows__button layout-arrows__button--left"
+    >
       <ArrowLeft />
-    </button>
-    <button class="layout-arrows__button layout-arrows__button--right">
+    </RouterLink>
+
+    <span
+      v-else
+      class="layout-arrows__button layout-arrows__button--left layout-arrows__button--disabled"
+    >
+      <ArrowLeft />
+    </span>
+
+    <RouterLink
+      v-if="nextPage"
+      :to="nextPage.path"
+      class="layout-arrows__button layout-arrows__button--right"
+    >
       <ArrowRight />
-    </button>
+    </RouterLink>
+
+    <span
+      v-else
+      class="layout-arrows__button layout-arrows__button--right layout-arrows__button--disabled"
+    >
+      <ArrowRight />
+    </span>
   </nav>
 </template>
 
 <script setup lang="ts">
-import { ArrowLeft, ArrowRight } from '@lucide/vue'
+import { ArrowLeft, ArrowRight } from "@lucide/vue";
+import { computed } from "vue";
+import { RouterLink, useRoute, useRouter } from "vue-router";
+
+const route = useRoute();
+const router = useRouter();
+
+const routes = computed(() =>
+  router
+    .getRoutes()
+    .filter((item) => item.meta.showInNavigation)
+    .sort((a, b) => Number(a.meta.order) - Number(b.meta.order))
+    .map((item) => ({
+      path: item.path,
+      label: item.meta.label,
+    })),
+);
+
+const currentPageIndex = computed(() => routes.value.findIndex((item) => route.path === item.path));
+
+const previosPage = computed(() => {
+  if (currentPageIndex.value <= 0) {
+    return null;
+  }
+  return routes.value[currentPageIndex.value - 1];
+});
+
+const nextPage = computed(() => {
+  if (currentPageIndex.value === -1 || currentPageIndex.value >= routes.value.length - 1) {
+    return null;
+  }
+  return routes.value[currentPageIndex.value + 1];
+});
 </script>
 
 <style scoped lang="scss">
