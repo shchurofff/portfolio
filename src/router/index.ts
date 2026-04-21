00a4437b124101f19df69ba378589a250044fa8f
Oleg Shchurov <o.shchurov@firecode-team.ru>
2026-04-21 22:08:24 +0300

    feat(route): created empty pages and added them in router

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,6 +2,8 @@ import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "@/views/HomeView.vue";
 import PageLayout from "@/layouts/PageLayout.vue";
 import AboutView from "@/views/AboutView.vue";
+import PetProjectsView from "@/views/PetProjectsView.vue";
+import ResumeView from "@/views/ResumeView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -10,10 +12,52 @@ const router = createRouter({
       path: "/",
       name: "root",
       component: PageLayout,
+      meta: {
+        label: "Layout",
+        order: 0,
+        showInNavigation: false,
+      },
       children: [
-        { path: "", redirect: "/home" },
-        { path: "home", name: "home", component: HomeView },
-        { path: "aboute", name: "aboute", component: AboutView },
+        {
+          path: "home",
+          name: "home",
+          component: HomeView,
+          meta: {
+            label: "Home",
+            order: 1,
+            showInNavigation: true,
+          },
+        },
+        {
+          path: "about",
+          name: "about",
+          component: AboutView,
+          meta: {
+            label: "About",
+            order: 2,
+            showInNavigation: true,
+          },
+        },
+        {
+          path: "pet-projects",
+          name: "pet-projects",
+          component: PetProjectsView,
+          meta: {
+            label: "Pet Projects",
+            order: 3,
+            showInNavigation: true,
+          },
+        },
+        {
+          path: "resume",
+          name: "resume",
+          component: ResumeView,
+          meta: {
+            label: "Resume",
+            order: 4,
+            showInNavigation: true,
+          },
+        },
       ],
     },
   ],
