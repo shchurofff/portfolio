3a80c17d5aa89eb6e91ea49d82daa3fbb4962b01
Oleg Shchurov <shchurov.oleg27@gmail.com>
2026-04-24 16:07:48 +0300

    fix(route): added redirect in root page

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,59 +1,60 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '@/views/HomeView.vue'
-import PageLayout from '@/layouts/PageLayout.vue'
-import AboutView from '@/views/AboutView.vue'
-import PetProjectsView from '@/views/PetProjectsView.vue'
-import ResumeView from '@/views/ResumeView.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "@/views/HomeView.vue";
+import PageLayout from "@/layouts/PageLayout.vue";
+import AboutView from "@/views/AboutView.vue";
+import PetProjectsView from "@/views/PetProjectsView.vue";
+import ResumeView from "@/views/ResumeView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'root',
+      path: "/",
+      name: "root",
       component: PageLayout,
+      redirect: "home",
       meta: {
-        label: 'Layout',
+        label: "Layout",
         order: 0,
         showInNavigation: false,
       },
       children: [
         {
-          path: 'home',
-          name: 'home',
+          path: "home",
+          name: "home",
           component: HomeView,
           meta: {
-            label: 'Home',
+            label: "Home",
             order: 1,
             showInNavigation: true,
           },
         },
         {
-          path: 'about',
-          name: 'about',
+          path: "about",
+          name: "about",
           component: AboutView,
           meta: {
-            label: 'About',
+            label: "About",
             order: 2,
             showInNavigation: true,
           },
         },
         {
-          path: 'pet-projects',
-          name: 'pet-projects',
+          path: "pet-projects",
+          name: "pet-projects",
           component: PetProjectsView,
           meta: {
-            label: 'Pet Projects',
+            label: "Pet Projects",
             order: 3,
             showInNavigation: true,
           },
         },
         {
-          path: 'resume',
-          name: 'resume',
+          path: "resume",
+          name: "resume",
           component: ResumeView,
           meta: {
-            label: 'Resume',
+            label: "Resume",
             order: 4,
             showInNavigation: true,
           },
@@ -61,6 +62,6 @@ const router = createRouter({
       ],
     },
   ],
-})
+});
 
-export default router
+export default router;
